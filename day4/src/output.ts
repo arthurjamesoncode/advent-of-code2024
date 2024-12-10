189cf9b387d45def7c2ebd34a226d2c0b9958a0c
Arthur <arthurj17@gmail.com>
2024-12-10 15:00:10 +0000

    day4 part2

--- day4/src/output.ts
+++ day4/src/output.ts
@@ -9,7 +9,7 @@ function getResult(func: Function, input: FormattedInput, expected: number) {
 
 function getOutputString() : string {
   const parts = [part1, part2];
-  const testCases = [18, 2578, 0, 0];
+  const testCases = [18, 2578, 9, 1972];
   let testIndex = 0;
 
   let i = 1;
--- day4/src/part2.ts
+++ day4/src/part2.ts
@@ -1,5 +1,85 @@
 import { FormattedInput } from './parseInput';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+const DIRECTIONS: [number, number][] = [
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
+
+export default function main(input: FormattedInput): number {
+  const checked = new Set<string>();
+
+  let total = 0;
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      if (input[row][col] === 'M') {
+        total += checkX([row, col], checked, input);
+      }
+    }
+  }
+
+  return total;
+}
+
+function checkX(
+  pos: [number, number],
+  checked: Set<string>,
+  input: FormattedInput
+): number {
+  let total = 0;
+  const [row, col] = pos;
+
+  for (const direction of DIRECTIONS) {
+    if (checked.has(`${row},${col},${direction}`)) continue;
+
+    const check1 = checkMas([row, col], direction, input);
+    checked.add(`${row},${col},${direction}`);
+    if (!check1 === true) continue;
+
+    let newRow = row + direction[0] * 2;
+    let newCol = col;
+    let newDirection: [number, number] = [-direction[0], direction[1]];
+    if (checked.has(`${newRow},${newCol},${newDirection}`)) continue;
+
+    let check2 = checkMas([newRow, newCol], newDirection, input);
+    checked.add(`${newRow},${newCol},${newDirection}`);
+    if (check2) {
+      total++;
+      continue;
+    }
+
+    newRow = row;
+    newCol = col + direction[1] * 2;
+    newDirection = [direction[0], -direction[1]];
+    if (checked.has(`${newRow},${newCol},${newDirection}`)) continue;
+
+    check2 = checkMas([newRow, newCol], newDirection, input);
+    checked.add(`${newRow},${newCol},${newDirection}`);
+    if (check2) {
+      total += 1;
+    }
+  }
+
+  return total;
+}
+
+function checkMas(
+  pos: [number, number],
+  direction: [number, number],
+  input: FormattedInput
+): boolean {
+  let CHECK = 'MAS';
+  let [currRow, currCol] = pos;
+  let count = 0;
+  for (const char of CHECK) {
+    if (input[currRow] && input[currRow][currCol] === char) {
+      currRow += direction[0];
+      currCol += direction[1];
+      count++;
+    }
+  }
+
+  return count === 3;
 }
