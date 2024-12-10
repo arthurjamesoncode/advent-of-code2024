3b036855efadb290e4901cdc9af002cda72f924b
Arthur <arthurj17@gmail.com>
2024-12-10 21:47:44 +0000

    day10 part2

--- day10/src/part1.ts
+++ day10/src/part1.ts
@@ -1,6 +1,6 @@
 import { FormattedInput } from './parseInput';
 
-const DIRECTIONS = [
+export const DIRECTIONS = [
   [1, 0],
   [0, 1],
   [-1, 0],
--- day10/src/part2.ts
+++ day10/src/part2.ts
@@ -1,5 +1,48 @@
 import { FormattedInput } from './parseInput';
+import { DIRECTIONS } from './part1';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+export default function main(input: FormattedInput): number {
+  let total = 0;
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      if (input[row][col] === 0) {
+        const score = calculateScore([row, col], input);
+        total += score;
+      }
+    }
+  }
+
+  return total;
+}
+
+function calculateScore(
+  trailhead: [number, number],
+  matrix: number[][]
+): number {
+  let paths = 0;
+  const stack = [trailhead];
+
+  while (stack.length) {
+    const pos = stack.pop()!;
+    const [row, col] = pos;
+    const height = matrix[row][col];
+
+    if (height === 9) {
+      paths++
+      continue;
+    }
+
+    for (const direction of DIRECTIONS) {
+      const newRow = row + direction[0];
+      const newCol = col + direction[1];
+
+      if (!matrix[newRow] || !matrix[newRow][newCol]) continue;
+
+      const newHeight = matrix[newRow][newCol];
+      if (newHeight - height === 1) stack.push([newRow, newCol]);
+    }
+  }
+
+  return paths;
 }
