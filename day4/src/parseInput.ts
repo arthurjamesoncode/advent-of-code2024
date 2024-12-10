496a7502b7e153b698e121619656653f5b34c667
Arthur <arthurj17@gmail.com>
2024-12-10 14:22:58 +0000

    day4 parseInput

--- day4/src/parseInput.ts
+++ day4/src/parseInput.ts
@@ -3,10 +3,10 @@ import fs from 'fs';
 const rawTest = fs.readFileSync('./inputs/test.txt', 'utf-8')
 const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
 
-export type FormattedInput = string //string is placeholder
+export type FormattedInput = string[][]
 
 function parseInput(input: string): FormattedInput {
-  return input
+  return input.split('\n').map(line => line.split(''))
 }
 
 export const test = parseInput(rawTest)
