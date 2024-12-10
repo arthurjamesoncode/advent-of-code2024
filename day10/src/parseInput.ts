b7faf5b3344f9736980ee3a20c73035bacd19691
Arthur <arthurj17@gmail.com>
2024-12-10 21:26:16 +0000

    day10 parseInput

--- day10/src/parseInput.ts
+++ day10/src/parseInput.ts
@@ -1,13 +1,15 @@
 import fs from 'fs';
 
-const rawTest = fs.readFileSync('./inputs/test.txt', 'utf-8')
+const rawTest = fs.readFileSync('./inputs/test.txt', 'utf-8');
 const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
 
-export type FormattedInput = string //string is placeholder
+export type FormattedInput = number[][];
 
 function parseInput(input: string): FormattedInput {
-  return input
+  const matrix = input.split('\n').map((row) => row.split('').map(Number));
+
+  return matrix
 }
 
-export const test = parseInput(rawTest)
-export const input = parseInput(rawInput)
+export const test = parseInput(rawTest);
+export const input = parseInput(rawInput);
