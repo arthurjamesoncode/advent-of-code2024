2b7bf32fcf1136449e562772f0ba107735978138
Arthur <arthurj17@gmail.com>
2024-12-10 21:45:02 +0000

    day10 part1

--- day10/src/part1.ts
+++ day10/src/part1.ts
@@ -1,5 +1,54 @@
 import { FormattedInput } from './parseInput';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+const DIRECTIONS = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
+
+export default function main(input: FormattedInput): number {
+  let total = 0;
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      if (input[row][col] === 0) {
+        const score = calculateScore([row, col], input);
+        total += score;
+      }
+    }
+  }
+
+  return total;
+}
+
+function calculateScore(
+  trailhead: [number, number],
+  matrix: number[][]
+): number {
+  const visitedPeaks = new Set();
+  const stack = [trailhead];
+
+  while (stack.length) {
+    const pos = stack.pop()!;
+    const [row, col] = pos;
+    const height = matrix[row][col];
+
+    if (height === 9) {
+      visitedPeaks.add(pos.join(','));
+      continue;
+    }
+
+    for (const direction of DIRECTIONS) {
+      const newRow = row + direction[0];
+      const newCol = col + direction[1];
+
+      if (!matrix[newRow] || !matrix[newRow][newCol]) continue;
+
+      const newHeight = matrix[newRow][newCol];
+      if (newHeight - height === 1) stack.push([newRow, newCol]);
+    }
+  }
+
+  return visitedPeaks.size;
 }
