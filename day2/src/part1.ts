a69616cc444ef007eb2f1c0f31b0d2fdd0be78a7
Arthur <arthurj17@gmail.com>
2024-12-10 13:12:12 +0000

    day2 part1

--- day2/src/part1.ts
+++ day2/src/part1.ts
@@ -1,5 +1,27 @@
 import { FormattedInput } from './parseInput';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+export default function main(input: FormattedInput): number {
+  let total = 0
+  for(const report of input) {
+    const safe = isReportSafe(report);
+    
+    if(safe) total++
+  }
+  return total;
+}
+
+function isReportSafe(report: number[]): boolean {
+  let increasing = false;
+  for (let i = 0; i < report.length - 1; i++) {
+    const diff = report[i] - report[i + 1];
+    if (diff === 0 || Math.abs(diff) > 3) return false;
+
+    if (i === 0 && diff > 0) {
+      increasing = true;
+      continue;
+    }
+
+    if (diff > 0 !== increasing) return false;
+  }
+  return true;
 }
