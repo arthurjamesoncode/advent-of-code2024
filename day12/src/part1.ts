a03b13344974711ae0c855f343d96be7a2c82272
Arthur <arthurj17@gmail.com>
2024-12-12 07:36:58 +0000

    refactor part1

--- day12/src/part1.ts
+++ day12/src/part1.ts
@@ -1,6 +1,6 @@
 import { FormattedInput } from './parseInput';
 
-const DIRECTIONS = [
+export const DIRECTIONS = [
   [1, 0],
   [-1, 0],
   [0, 1],
@@ -16,40 +16,49 @@ export default function main(input: FormattedInput): number {
       const pos = `${row},${col}`;
       if (visited.has(pos)) continue;
 
-      const plot = input[row][col];
-      const stack: [number, number][] = [[row, col]];
-      let area = 1;
-      let perimeter = 4;
-
-      while (stack.length) {
-        const [row, col] = stack.pop()!;
-        const pos = `${row},${col}`;
-        visited.add(pos);
-
-        for (const direction of DIRECTIONS) {
-          const newRow = row + direction[0];
-          const newCol = col + direction[1];
-          const newPos = `${newRow},${newCol}`;
-
-          if (!input[newRow] || input[newRow][newCol] !== plot) continue;
-
-          if (visited.has(newPos)) {
-            perimeter -= 1;
-            continue;
-          }
-
-          visited.add(newPos);
-
-          area += 1;
-
-          perimeter--; //separate lines to make the algorithm more obvious
-          perimeter += 4;
-          stack.push([newRow, newCol]);
-        }
-      }
-
+      const { area, perimeter } = calculateRegion([row, col], input, visited);
       total += area * perimeter;
     }
   }
   return total;
+}
+
+function calculateRegion(
+  pos: [number, number],
+  matrix: string[][],
+  visited: Set<string>
+) {
+  const [row, col] = pos;
+  const plot = matrix[row][col];
+  const stack: [number, number][] = [[row, col]];
+  let area = 1;
+  let perimeter = 4;
+
+  while (stack.length) {
+    const [row, col] = stack.pop()!;
+    const pos = `${row},${col}`;
+    visited.add(pos);
+
+    for (const direction of DIRECTIONS) {
+      const newRow = row + direction[0];
+      const newCol = col + direction[1];
+      const newPos = `${newRow},${newCol}`;
+
+      if (!matrix[newRow] || matrix[newRow][newCol] !== plot) continue;
+
+      if (visited.has(newPos)) {
+        perimeter -= 1;
+        continue;
+      }
+
+      visited.add(newPos);
+
+      area += 1;
+
+      perimeter--; //separate lines to make the algorithm more obvious
+      perimeter += 4;
+      stack.push([newRow, newCol]);
+    }
+  }
+  return { area, perimeter };
 }
