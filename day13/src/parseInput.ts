8835489a31567ece86ede0d08e9ec78a6cad8dcf
Arthur <arthurj17@gmail.com>
2024-12-12 19:04:06 +0000

    init day13

---
+++ day13/src/parseInput.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+export type FormattedInput = string[][];
+
+function parseInput(input: string): FormattedInput {
+  const matrix = input.split('\n').map((line) => line.split(''));
+  return matrix;
+}
+
+function getRawTests(numOfTests: number) {
+  const rawTests = [];
+  for (let i = 1; i <= numOfTests; i++) {
+    rawTests.push(fs.readFileSync('./inputs/test' + i + '.txt', 'utf-8'));
+  }
+  return rawTests
+}
+
+const rawTests = getRawTests(1)
+const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
+
+
+export const tests = rawTests.map(parseInput);
+export const input = parseInput(rawInput);
