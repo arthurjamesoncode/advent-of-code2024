ab976257ec2925790cc6906b336b0fa9aa519310
Arthur <arthurj17@gmail.com>
2024-12-13 08:55:53 +0000

    day13 parseInput

--- day13/src/parseInput.ts
+++ day13/src/parseInput.ts
@@ -1,10 +1,33 @@
 import fs from 'fs';
 
-export type FormattedInput = string[][];
+export type FormattedInput = {
+  a: [number, number];
+  b: [number, number];
+  prize: [number, number];
+}[];
 
 function parseInput(input: string): FormattedInput {
-  const matrix = input.split('\n').map((line) => line.split(''));
-  return matrix;
+  const machines = input.split('\n\n').map((machine) => {
+    const [lineA, lineB, prizeLine] = machine.split('\n');
+    const a = lineA
+      .substring(10)
+      .split(', ')
+      .map((direction) => direction.substring(2))
+      .map(Number) as [number, number];
+    const b = lineB
+      .substring(10)
+      .split(', ')
+      .map((direction) => direction.substring(2))
+      .map(Number) as [number, number];
+    const prize = prizeLine
+      .substring(7)
+      .split(', ')
+      .map((direction) => direction.substring(2))
+      .map(Number) as [number, number];
+
+    return { a, b, prize };
+  });
+  return machines;
 }
 
 function getRawTests(numOfTests: number) {
@@ -12,12 +35,11 @@ function getRawTests(numOfTests: number) {
   for (let i = 1; i <= numOfTests; i++) {
     rawTests.push(fs.readFileSync('./inputs/test' + i + '.txt', 'utf-8'));
   }
-  return rawTests
+  return rawTests;
 }
 
-const rawTests = getRawTests(1)
+const rawTests = getRawTests(1);
 const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
-
 
 export const tests = rawTests.map(parseInput);
 export const input = parseInput(rawInput);
