898bd6092ee4340c1f2195166eca5d7fe5144625
Arthur <arthurj17@gmail.com>
2024-12-15 10:56:42 +0000

    day15 parseInput

--- day15/src/parseInput.ts
+++ day15/src/parseInput.ts
@@ -1,9 +1,13 @@
 import fs from 'fs';
 
-export type FormattedInput = string; //placeholder type
+export type FormattedInput = { matrix: string[][]; moves: string };
 
 function parseInput(input: string): FormattedInput {
-  return input;
+  const [warehouse, instructions] = input.split('\n\n');
+  const matrix = warehouse.split('\n').map((row) => row.split(''));
+  const moves = instructions.split('\n').join('');
+  
+  return { matrix, moves };
 }
 
 function getRawTests(numOfTests: number) {
@@ -11,12 +15,11 @@ function getRawTests(numOfTests: number) {
   for (let i = 1; i <= numOfTests; i++) {
     rawTests.push(fs.readFileSync('./inputs/test' + i + '.txt', 'utf-8'));
   }
-  return rawTests
+  return rawTests;
 }
 
-const rawTests = getRawTests(1)
+const rawTests = getRawTests(1);
 const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
-
 
 export const tests = rawTests.map(parseInput);
 export const input = parseInput(rawInput);
