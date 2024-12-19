4cf2d7b07e0722ebba0ea667f5fa8dbc8c4b4d7f
Arthur <arthurj17@gmail.com>
2024-12-19 10:31:32 +0000

    day19 parseInput

--- day19/src/parseInput.ts
+++ day19/src/parseInput.ts
@@ -1,9 +1,12 @@
 import fs from 'fs';
 
-export type FormattedInput = string; //placeholder type
+export type FormattedInput = { designs: string[]; towels: string[] }; //placeholder type
 
 function parseInput(input: string): FormattedInput {
-  return input;
+  const [towels, designs] = input
+    .split('\n\n')
+    .map((section, i) => (i === 0 ? section.split(', ') : section.split('\n')));
+  return { designs, towels };
 }
 
 function getRawTests(numOfTests: number) {
@@ -11,12 +14,11 @@ function getRawTests(numOfTests: number) {
   for (let i = 1; i <= numOfTests; i++) {
     rawTests.push(fs.readFileSync('./inputs/test' + i + '.txt', 'utf-8'));
   }
-  return rawTests
+  return rawTests;
 }
 
-const rawTests = getRawTests(1)
+const rawTests = getRawTests(1);
 const rawInput = fs.readFileSync('./inputs/input.txt', 'utf-8');
-
 
 export const tests = rawTests.map(parseInput);
 export const input = parseInput(rawInput);
