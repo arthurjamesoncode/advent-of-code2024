2ffb66556d6a5bf5f87a0b080c10570e5ccd9906
Arthur <arthurj17@gmail.com>
2024-12-19 11:23:45 +0000

    day19 part2

--- day19/src/part2.ts
+++ day19/src/part2.ts
@@ -1,5 +1,41 @@
 import { FormattedInput } from './parseInput';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+export default function main(input: FormattedInput): number {
+  const { designs, towels } = input;
+
+  const checked = new Map<string, number>();
+  let total = 0;
+  for (const design of designs) {
+    const add = countDesigns(design, 0, towels, checked);
+    total += add;
+  }
+
+  return total;
+}
+
+function countDesigns(
+  design: string,
+  index: number,
+  towels: string[],
+  checked: Map<string, number>
+) {
+  if (index === design.length) {
+    return 1;
+  }
+
+  const substring = design.substring(index);
+  if (checked.has(substring)) return checked.get(substring)!;
+
+  let total = 0;
+  for (const towel of towels) {
+    const match = design.substring(index, index + towel.length) === towel;
+
+    if (match) {
+      const num = countDesigns(design, index + towel.length, towels, checked);
+      total += num
+    }
+  }
+
+  checked.set(substring, total);
+  return total;
 }
