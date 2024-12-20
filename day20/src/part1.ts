6e2fd5728f18cbcbecf9932f61bc09138b3b6a58
Arthur <arthurj17@gmail.com>
2024-12-20 21:39:21 +0000

    optimized part1

--- day20/src/part1.ts
+++ day20/src/part1.ts
@@ -11,27 +11,9 @@ export default function main(input: FormattedInput): number {
   const distances = getDistanceMap(input);
   const cheats = new Map<number, number>(); //seconds saved -> count
 
-  let pos = findCell('S', input);
-  const visited = new Set<string>();
+  for (const [key, distance] of distances) {
+    const [row, col] = key.split(',').map(Number);
 
-  const stack: [number, number][] = [pos];
-  while (stack.length) {
-    const pos = stack.pop()!;
-    const key = pos.join(',');
-
-    if (visited.has(key)) continue;
-    visited.add(key);
-
-    const [row, col] = pos;
-    const char = input[row][col];
-
-    if (char === 'E') {
-      break;
-    }
-
-    const distance = distances.get(key)!;
-
-    const newPositions: [string, number][] = [];
     for (const direction of DIRECTIONS) {
       const [dRow, dCol] = direction;
       let [newRow, newCol] = [row + dRow, col + dCol];
@@ -39,35 +21,30 @@ export default function main(input: FormattedInput): number {
       for (const direction of DIRECTIONS) {
         const [dRow, dCol] = direction;
         const newPos: [number, number] = [newRow + dRow, newCol + dCol];
-        if (input[newPos[0]] && input[newPos[0]][newPos[1]] !== '#')
-          newPositions.push([newPos.join(','), distance + 2]);
-      }
-    }
 
-    for (const [key, distance] of newPositions) {
-      const current = distances.get(key);
-      if (current && distance < current) {
-        const numCheats = cheats.get(distance - current);
+        if (!input[newPos[0]] || input[newPos[0]][newPos[1]] === '#') {
+          continue;
+        }
+
+        const newDistance = distance + 2;
+        const newKey = newPos.join(',');
+        const current = distances.get(newKey);
+
+        if (!current || current < newDistance) {
+          continue;
+        }
+
+        const numCheats = cheats.get(current - newDistance);
         numCheats
-          ? cheats.set(distance - current, numCheats + 1)
-          : cheats.set(distance - current, 1);
-      }
-    }
-
-    for (const direction of DIRECTIONS) {
-      const [dRow, dCol] = direction;
-      const newPos: [number, number] = [row + dRow, col + dCol];
-
-      const [newRow, newCol] = newPos;
-      if (input[newRow] && input[newRow][newCol] !== '#') {
-        stack.push(newPos);
+          ? cheats.set(current - newDistance, numCheats + 1)
+          : cheats.set(current - newDistance, 1);
       }
     }
   }
 
   let total = 0;
   for (const [key, value] of cheats) {
-    if (key > -100) continue;
+    if (key < 100) continue;
 
     total += value;
   }
