2a20046d14c60952f7b96e6e10631eff2bdb1435
Arthur <arthurj17@gmail.com>
2024-12-20 22:14:03 +0000

    refactor day20 part2

--- day20/src/part2.ts
+++ day20/src/part2.ts
@@ -1,100 +1,48 @@
 import { FormattedInput } from './parseInput';
-import { DIRECTIONS, findCell, getDistanceMap } from './part1';
+import { DIRECTIONS, getDistanceMap } from './part1';
 import { MinPriorityQueue } from '@datastructures-js/priority-queue';
 
 export default function main(input: FormattedInput): number {
   const distances = getDistanceMap(input);
-  let cheats = new Map<number, number>(); //seconds saved -> count
+  const path = getPath(distances);
 
-  let pos = findCell('S', input);
-  const visited = new Set<string>();
-
-  const stack: [number, number][] = [pos];
-  while (stack.length) {
-    const pos = stack.pop()!;
-    const key = pos.join(',');
-
-    if (visited.has(key)) continue;
-    visited.add(key);
-
-    const [row, col] = pos;
-    const char = input[row][col];
-
-    if (char === 'E') {
-      break;
-    }
-    const distance = distances.get(key);
-    if (distance === undefined) {
-      throw new Error('Incorrect Distance Map');
-    }
-
-    cheats = startCheat(pos, distance, distances, cheats, input);
-
-    for (const direction of DIRECTIONS) {
-      const [dRow, dCol] = direction;
-      const newPos: [number, number] = [row + dRow, col + dCol];
-
-      const [newRow, newCol] = newPos;
-      if (input[newRow] && input[newRow][newCol] !== '#') {
-        stack.push(newPos);
-      }
-    }
-  }
+  const cheats = new Map<number, number>();
 
   let total = 0;
-  for (const [key, value] of cheats) {
-    if (key < 100) continue;
+  for (let i = 0; i < path.length - 1; i++) {
+    const key1 = path[i];
+    const distance1 = distances.get(key1)!;
+    const pos1 = key1.split(',').map(Number) as [number, number];
 
-    total += value;
+    for (let j = i; j < path.length; j++) {
+      const key2 = path[j];
+      const distance2 = distances.get(key2)!;
+      const pos2 = key2.split(',').map(Number) as [number, number];
+
+      const manhattanDistance = getManhattanDistance(pos1, pos2);
+      if (manhattanDistance > 20) continue;
+
+      if (distance1 + manhattanDistance >= distance2) continue;
+
+      const diff = distance2 - (distance1 + manhattanDistance);
+      const numCheats = cheats.get(diff);
+      numCheats ? cheats.set(diff, numCheats + 1) : cheats.set(diff, 1);
+
+      if (diff >= 100) total++;
+    }
   }
 
   return total;
 }
 
-function startCheat(
-  pos: [number, number],
-  distance: number,
-  distances: Map<string, number>,
-  cheats: Map<number, number>,
-  matrix: string[][]
-) {
-  const maxDistance = distance + 20;
-  const visited = new Set('');
-
-  const queue = new MinPriorityQueue<[[number, number], number]>(
-    (item) => item[1]
-  );
-
-  queue.enqueue([pos, distance]);
-  while (!queue.isEmpty()) {
-    const [pos, distance] = queue.dequeue();
-    if (distance > maxDistance) continue;
-
-    const key = pos.join(',');
-    if (visited.has(key)) continue;
-    visited.add(key);
-
-    const [row, col] = pos;
-    for (const direction of DIRECTIONS) {
-      const [dRow, dCol] = direction;
-      const newPos: [number, number] = [row + dRow, col + dCol];
-
-      const [newRow, newCol] = newPos;
-      if (matrix[newRow] && matrix[newRow][newCol]) {
-        queue.enqueue([newPos, distance + 1]);
-      }
-    }
-
-    if (matrix[row][col] === '#') continue;
-
-    const current = distances.get(key);
-    if (current && current > distance) {
-      const numCheats = cheats.get(current - distance);
-      numCheats
-        ? cheats.set(current - distance, numCheats + 1)
-        : cheats.set(current - distance, 1);
-    }
+function getPath(distances: Map<string, number>) {
+  const path: string[] = [];
+  for (const entry of distances.keys()) {
+    path.push(entry);
   }
+  return path;
+}
 
-  return cheats;
+function getManhattanDistance(pos1: [number, number], pos2: [number, number]) {
+  return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
 }
