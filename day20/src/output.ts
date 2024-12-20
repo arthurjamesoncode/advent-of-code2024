9c0a467d62c72147d2bef6ed6c62e5d743499f9d
Arthur <arthurj17@gmail.com>
2024-12-20 11:56:29 +0000

    day20 part1

--- day20/src/output.ts
+++ day20/src/output.ts
@@ -10,10 +10,10 @@ function getResult(func: Function, input: FormattedInput, expected: number) {
 function getOutputString(): string {
   const parts = [part1, part2];
   const testCases = [
-    [NaN],
+    [0],
     [NaN],
   ];
-  const answers = [NaN, NaN];
+  const answers = [1452, NaN];
   let testIndex = 0;
 
   let i = 0;
