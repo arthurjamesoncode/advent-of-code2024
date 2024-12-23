13d3061ecf68987f16f384a03fe38742c7bfd60f
Arthur <arthurj17@gmail.com>
2024-12-23 13:24:20 +0000

    day23 part1

--- day23/src/output.ts
+++ day23/src/output.ts
@@ -10,10 +10,10 @@ function getResult(func: Function, input: FormattedInput, expected: number) {
 function getOutputString(): string {
   const parts = [part1, part2];
   const testCases = [
-    [NaN],
+    [7],
     [NaN],
   ];
-  const answers = [NaN, NaN];
+  const answers = [1110, NaN];
   let testIndex = 0;
 
   let i = 0;
--- day23/src/part1.ts
+++ day23/src/part1.ts
@@ -1,5 +1,27 @@
 import { FormattedInput } from './parseInput';
 
-export default function main(input: FormattedInput): FormattedInput {
-  return input;
+export default function main(input: FormattedInput): number {
+  let total = 0;
+  const sets = new Set<string>();
+
+  for (const [node, connections] of input) {
+    if (node[0] !== 't') continue;
+
+    for (let i = 0; i < connections.length - 1; i++) {
+      for (let j = i + 1; j < connections.length; j++) {
+        const a = connections[i];
+        const b = connections[j];
+        const selection = [node, a, b].sort().join(',');
+
+        if(sets.has(selection)) continue;
+        sets.add(selection)
+
+        if (input.get(a)?.includes(b)) {
+          total++;
+        }
+      }
+    }
+  }
+
+  return total;
 }
